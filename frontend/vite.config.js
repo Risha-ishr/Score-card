f21a3b83c65204281e73887e50647ebc7a2a294b
Risha-ishr <risha@ishrpeople.com>
2026-07-06 12:23:11 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': 'http://localhost:8000'
+      '/api': 'https://score-card-production.up.railway.app'
     }
   }
 });
